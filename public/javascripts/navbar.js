892ef7ec17a62fb504b12c962c1ab4a5074b18c8
Kirill <neikila@gmail.com>
2016-01-11 22:07:27 +0300

    Redone purchase to accurate js with object.

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,49 +1,97 @@
 /**
  * Created by neikila on 10.01.16.
  */
-var auth = function() {
-    if(sessionStorage.getItem("isAuth") == undefined) {
-        console.log("Request to get userData");
-        $.ajax({
-            type: "GET",
-            url: "/userData",
-            data: {},
-            success: function(json){
-                console.log(json);
-                if (json.isAuth) {
-                    sessionStorage.setItem("isAuth", true);
-                    sessionStorage.setItem("login", json.user.login);
-                    sessionStorage.setItem("name", json.user.name);
-                    updateNavbarRight();
-                } else {
-                    sessionStorage.setItem("isAuth", false);
+var userConstructor = function() {
+    var user = {};
+
+    user._isInitialised = false;
+    user.login = undefined;
+    user.email = undefined;
+    user.name = undefined;
+    user.surname = undefined;
+
+    function init(login, email, name, surname) {
+        user._isInitialised = true;
+        if(arguments.length != 0) {
+            user.login = login;
+            user.email = email;
+            user.name = name;
+            user.surname = surname;
+            user.auth();
+        }
+    }
+
+    user._isAuthorised = false;
+    user.isAuth = function() {
+        return user._isAuthorised
+    };
+
+    user.auth = function () {
+        user._isAuthorised = true;
+        // TODO some action
+        navbarController.invalidate();
+    };
+
+    user.deauth = function() {
+        user._isAuthorised = false;
+        // TODO some action
+        navbarController.invalidate();
+    };
+
+    return user;
+};
+
+var user = userConstructor();
+
+var navbarController = function() {
+    function controller() {}
+
+    controller.auth = function() {
+        if(sessionStorage.getItem("isAuth") == undefined) {
+            console.log("Request to get userData");
+            $.ajax({
+                type: "GET",
+                url: "/userData",
+                data: {},
+                success: function(json){
+                    console.log(json);
+                    if (json.isAuth) {
+                        sessionStorage.setItem("isAuth", true);
+                        sessionStorage.setItem("login", json.user.login);
+                        sessionStorage.setItem("name", json.user.name);
+                        controller.updateRight();
+                    } else {
+                        sessionStorage.setItem("isAuth", false);
+                    }
                 }
-            }
-        });
-    }
-};
+            });
+        }
+    };
 
-var invalidate = function() {
-    sessionStorage.removeItem("isAuth");
-    sessionStorage.removeItem("login");
-    sessionStorage.removeItem("name");
-};
+    controller.updateRight = function() {
+        var navbarRight = $("ul.navbar-right");
+        var isAuth = sessionStorage.getItem("isAuth") == "true";
+        if (isAuth) {
+            sessionStorage.getItem("login");
+            navbarRight.empty();
+            navbarRight.append('<p class="navbar-text">Signed in as <a href="#" class="navbar-link">' + sessionStorage.getItem("name") +
+                '</a></p><li><a href="/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>');
+            $("ul.navbar-right li a").click(controller.invalidate());
+        } else {
+            navbarRight.empty();
+            navbarRight.append('<li><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>' +
+                '<li><a href="/signup"><span class="glyphicon glyphicon-user"></span> Signup</a></li>');
+            controller.auth();
+        }
+    };
 
-var updateNavbarRight = function() {
-    var navbarRight = $("ul.navbar-right");
-    var isAuth = sessionStorage.getItem("isAuth") == "true";
-    if (isAuth) {
-        sessionStorage.getItem("login");
-        navbarRight.empty();
-        navbarRight.append('<p class="navbar-text">Signed in as <a href="#" class="navbar-link">' + sessionStorage.getItem("name") +
-            '</a></p><li><a href="/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>');
-        $("ul.navbar-right li a").click(invalidate());
-    } else {
-        navbarRight.empty();
-        navbarRight.append('<li><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>' +
-            '<li><a href="/signup"><span class="glyphicon glyphicon-user"></span> Signup</a></li>');
-        auth();
-    }
-};
+    controller.invalidate = function() {
+        sessionStorage.removeItem("isAuth");
+        sessionStorage.removeItem("login");
+        sessionStorage.removeItem("name");
+    };
 
-updateNavbarRight();
+    return controller
+}();
+
+navbarController.updateRight();
---
+++ public/javascripts/purchase.js
@@ -0,0 +1,140 @@
+/**
+ * Created by neikila on 11.01.16.
+ */
+
+var purchasesPage = function() {
+    var groupName = undefined;
+    var pieChart = null;
+
+    function page(nextGroupName) {
+        console.log("Start loading page");
+        groupName = nextGroupName;
+        purchasesPage.getPurchasesFromServer();
+    }
+
+    page.getGroupName = function() {
+        return groupName;
+    };
+
+    page.setGroupName = function(groupNameParam) {
+        groupName = groupNameParam;
+        $(".panel-heading").text(groupName);
+    };
+
+    page.getGroupPieDataFromServer = function(num) {
+            $.ajax({
+                type: "GET",
+                url: "/getGroupPieData",
+                data: {
+                    "groupName": groupName
+                },
+                success: function(pieData){
+                    var graph = document.getElementById('graph').getContext('2d');
+                    var pieOptions = {
+                        animationSteps: num,
+                        animationEasing: 'easeInOutQuart'
+                    };
+                    if (pieChart != null) {
+                        pieChart.destroy();
+                    }
+                    pieChart = new Chart(graph).Pie(pieData, pieOptions);
+                    var list = $(document).xpathEvaluate("//ul[@aria-labelledby='product']");
+                    list.empty();
+                    pieData.forEach(function(group) {
+                        list.append("<li><a class='result' data-id='{{_id}}'>" + group.label + "</a></li>");
+                    });
+                    $(document).xpathEvaluate("//ul[@aria-labelledby='product']/li").click(function() {$("#product").val($(this).children().text())});
+                }
+            });
+        };
+
+    page.setListeners = function () {
+        $('#product').focusout(function(){
+            $("div.dropdown-backdrop").click();
+        });
+
+        $("#savePur").click(function(){
+            postJSON("/saveJSON", {
+                "product": $("#product").val(),
+                "amount": Number($("#amount").val()),
+                "groupName": groupName
+            }, function(json){
+                var num = $(document).xpathEvaluate("//table[contains(@class, 'table')]/tbody/\*").size() + 1;
+                if (num != 1) {
+                    $("<tr><th scope='row'>" + num + "</th><td>" + json.product + "</td><td>" + json.amount + "</td></tr>")
+                        .insertBefore($(document).xpathEvaluate("//table[contains(@class, 'table')]/tbody/\*[1]"));
+                } else {
+                    $(document).xpathEvaluate("//table[contains(@class, 'table')]/tbody")
+                        .append("<tr><th scope='row'>" + num + "</th><td>" + json.product + "</td><td>" + json.amount + "</td></tr>");
+                }
+                page.getGroupPieDataFromServer(0);
+            });
+            return false;
+        });
+
+        $(window).on("resize", function(event) {
+            page.resize();
+        });
+    };
+
+    page.resize = function() {
+        var pie = $("canvas");
+        var pieholder = $("div.pieholder");
+        var pieLength = pieholder.width();
+        if (pieLength > 400) {
+            pieLength = 400
+        }
+        pieholder.empty().append("<canvas id='graph'></canvas>");
+        pieChart = null;
+        page.setPieSize();
+        page.getGroupPieDataFromServer(0);
+    };
+
+    page.getPurchasesFromServer = function() {
+            var requestData = {};
+            if (groupName != undefined) {
+                requestData = {
+                    "groupName": groupName
+                }
+            }
+            $.ajax({
+                type: "GET",
+                url: "/purchasesJSON",
+                data: requestData,
+                success: function(json){
+                    page.setGroupName(json.groupName);
+                    var tableEl = $("tbody");
+                    var indexPurchase = 1;
+                    tableEl.empty();
+                    json.purchases.reverse().forEach(function(purchase) {
+                        tableEl.append("<tr>" +
+                            "<th scope='row'>" + indexPurchase + "</th>" +
+                            "<td>" + purchase.productName + " </td>" +
+                            "<td>" + purchase.amount + "</td>" +
+                            "</tr>");
+                        ++indexPurchase;
+                    });
+                    page.getGroupPieDataFromServer(100);
+                }
+            });
+        };
+
+    page.setPieSize = function() {
+        var pie = $("canvas");
+        var pieLength = $("div.pieholder").width();
+        if (pieLength > 400) {
+            pieLength = 400
+        }
+        pie.attr("width", pieLength);
+        pie.attr("height", pieLength);
+    };
+
+    return page;
+}();
+
+purchasesPage.setListeners();
+purchasesPage.setPieSize();
+
+$(document).ready(function () {
+    purchasesPage(undefined);
+});
