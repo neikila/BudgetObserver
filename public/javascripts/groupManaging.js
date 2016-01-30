cf0c39e82776e0807d800d2e292ea1b68d2f5efe
Kirill <neikila@gmail.com>
2016-01-30 23:33:53 +0300

    Group managing page somehow

--- public/javascripts/groupManaging.js
+++ public/javascripts/groupManaging.js
@@ -9,13 +9,16 @@ var test;
 groupManaging = function() {
     var manager = function() {};
 
-    var groupsEl = $("tbody");
+    var groupsEl = $(".container-fluid");
 
-    var glyphs = "<td>" +
-        "<span class='glyphicon glyphicon-info-sign'></span>" +
-        "<span class='glyphicon glyphicon-pencil'></span>" +
-        "<span class='glyphicon glyphicon-remove'></span>" +
-        "</td>";
+    var glyphs = "\
+    <div class='col-md-4 col-md-offset-1 button-area'>\
+        <div class='btn-group' role='group' aria-label='...'>\
+            <button type='button' class='btn btn-info'><span class='glyphicon glyphicon-info-sign'></span></button>\
+            <button type='button' class='btn btn-default'><span class='glyphicon glyphicon-pencil'></span></button>\
+            <button type='button' class='btn btn-danger'><span class='glyphicon glyphicon-remove'></span></button>\
+        </div>\
+    </div>";
 
 
     manager.init = function () {
@@ -25,16 +28,19 @@ groupManaging = function() {
             success: function(json){
                 console.log(json);
                 test = json;
-                var currentSize = $("tbody tr").size();
+                var currentSize = $(".row.panel").size();
                 json.forEach(function(group) {
                     ++currentSize;
                     var temp = group.dateOfCreating;
-                    groupsEl.append("<tr>" +
-                        "<th scope='row'>" + currentSize + "</th>" +
-                        "<td>" + group.groupName + "</td>" +
-                        "<td>" + group.author + "</td>" +
-                        "<td>" + temp.substring(0, temp.length - 2)+ "</td>" +
-                        glyphs + "</tr>")
+                    groupsEl.append("\
+                        <div class='row panel'>\
+                            <div class='col-md-7 area'>\
+                                <div class='col-sm-1 col-xs-1'><span>" + currentSize + "</span></div>\
+                                <div class='col-sm-3 col-xs-6'><span>" + group.groupName + "</span></div>\
+                                <div class='col-sm-2 col-xs-offset-1 col-xs-5'><span>" + group.author + "</span></div>\
+                                <div class='col-sm-4 col-xs-offset-1 col-xs-6'><span>" + temp.substring(0, temp.length - 2)+ "</span></div>\
+                            </div>" + glyphs + "\
+                        </div>")
                 })
             }
         });
@@ -42,5 +48,23 @@ groupManaging = function() {
 
     return manager;
 }();
+
+//$(".container-fluid").append("\
+//    <div class='row panel'>\
+//        <div class='col-md-7 area'>\
+//            <div class='col-sm-1 col-xs-1'><span>1</span></div>\
+//            <div class='col-sm-3 col-xs-6'><span>test</span></div>\
+//            <div class='col-sm-2 col-xs-offset-1 col-xs-5'><span>author</span></div>\
+//            <div class='col-sm-4 col-xs-offset-1 col-xs-6'><span>2014:12:12 10:11:04</span></div>\
+//        </div>\
+//        <div class='col-md-4 col-md-offset-1 button-area'>\
+//            <div class='btn-group' role='group' aria-label='...'>\
+//                <button type='button' class='btn btn-info'><span class='glyphicon glyphicon-info-sign'></span></button>\
+//                <button type='button' class='btn btn-default'><span class='glyphicon glyphicon-pencil'></span></button>\
+//                <button type='button' class='btn btn-danger'><span class='glyphicon glyphicon-remove'></span></button>\
+//            </div>\
+//        </div>\
+//    </div>\
+//");
 
 groupManaging.init();
